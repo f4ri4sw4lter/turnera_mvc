7c65d92ed4acdc17180af279f674f1edabe725e6
f4ri4sw4lter <fariaswalter95@gmail.com>
2021-03-15 18:38:13 -0300

    primer push

---
+++ views/turno/index.php
@@ -0,0 +1,28 @@
+<?php include 'views/header.php'; ?>
+
+  <!-- Navigation -->
+  <?php include 'views/menu.php';?>
+
+  <!-- Page Content -->
+<h1>TURNOS</h1>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 text-center">
+        <h1 class="mt-5">A Bootstrap 4 Starter Template</h1>
+        <p class="lead">Complete with pre-defined file paths and responsive navigation!</p>
+        <ul class="list-unstyled">
+          <li>Bootstrap 4.5.3</li>
+          <li>jQuery 3.5.1</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.slim.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
